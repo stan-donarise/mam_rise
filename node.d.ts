266b41b5497d70d9a1e103ca0486a28a89d1a589
stan-donarise <stan-donarise@users.noreply.github.com>
2025-06-26 07:24:33 +0000

    Deploying to gh-pages from @ stan-donarise/mam_rise@ffb09b506263262cd6ac5fed522459f0bedb0267 🚀

--- node.d.ts
+++ node.d.ts
@@ -5208,7 +5208,9 @@ declare namespace $ {
 declare namespace $ {
 
 	export class $mol_theme_auto extends $mol_plugin {
-		theme( ): string
+		dark( ): string
+		theme( ): ReturnType< $mol_theme_auto['dark'] >
+		light( ): string
 		attr( ): ({ 
 			'mol_theme': ReturnType< $mol_theme_auto['theme'] >,
 		}) 
@@ -5219,7 +5221,7 @@ declare namespace $ {
 //# sourceMappingURL=auto.view.tree.d.ts.map
 declare namespace $.$$ {
     class $mol_theme_auto extends $.$mol_theme_auto {
-        theme(): "$mol_theme_light" | "$mol_theme_dark";
+        theme(): string;
     }
 }
 
