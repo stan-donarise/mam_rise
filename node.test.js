d848a260e573a8ee244f583b67a9f9aea6119f40
stan-donarise <stan-donarise@users.noreply.github.com>
2025-09-09 07:22:30 +0000

    Deploying to gh-pages from @ stan-donarise/mam_rise@ffb09b506263262cd6ac5fed522459f0bedb0267 🚀

--- node.test.js
+++ node.test.js
@@ -8678,6 +8678,7 @@ var $;
 			return {
 				...(super.attr()), 
 				"src": (this.uri()), 
+				"title": (this.hint()), 
 				"alt": (this.title()), 
 				"loading": (this.loading()), 
 				"decoding": (this.decoding()), 
