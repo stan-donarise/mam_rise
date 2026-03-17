6b6e8f72538c777a313fb99dfa0a0e1ca960c824
stan-donarise <stan-donarise@users.noreply.github.com>
2026-03-17 07:56:16 +0000

    Deploying to gh-pages from @ stan-donarise/mam_rise@3141aad74733bf7e62ea5c93581c1f948c12f6f0 🚀

--- node.js
+++ node.js
@@ -8833,6 +8833,8 @@ var $;
                 }
                 catch (error) {
                     $mol_fail_log(error);
+                    if (error instanceof Error)
+                        return '💥' + error.message;
                     return '';
                 }
             }
--- web.js
+++ web.js
@@ -8476,6 +8476,8 @@ var $;
                 }
                 catch (error) {
                     $mol_fail_log(error);
+                    if (error instanceof Error)
+                        return '💥' + error.message;
                     return '';
                 }
             }
