7eef45a970640330b1caf9d5aef4b36131643327
stan-donarise <stan-donarise@users.noreply.github.com>
2026-03-24 07:56:57 +0000

    Deploying to gh-pages from @ stan-donarise/mam_rise@3141aad74733bf7e62ea5c93581c1f948c12f6f0 🚀

--- node.mjs
+++ node.mjs
@@ -7136,6 +7136,7 @@ var $;
 "use strict";
 var $;
 (function ($) {
+    let x = /x/[Symbol.matchAll];
     class $mol_regexp extends RegExp {
         groups;
         constructor(source, flags = 'gsu', groups = []) {
