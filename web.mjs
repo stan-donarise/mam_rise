05081943930ea6c77f897f4c91856584d87b8214
stan-donarise <stan-donarise@users.noreply.github.com>
2026-09-23 12:40:11 +0000

    Deploying to gh-pages from @ stan-donarise/mam_rise@db0afca56eb9f817889e90ac6e6cc26966739b90 🚀

--- web.mjs
+++ web.mjs
@@ -21591,6 +21591,8 @@ var $;
             for (const gift of this._gift.values()) {
                 if (mine.units_persisted.has(gift))
                     continue;
+                if (!this.unit_seal(gift))
+                    continue;
                 persisting.add(gift);
                 check_lord(gift.lord());
                 check_lord(gift.mate());
@@ -21599,6 +21601,8 @@ var $;
                 for (const units of kids.values()) {
                     for (const sand of units.values()) {
                         if ($mol_wire_sync(mine.units_persisted).has(sand))
+                            continue;
+                        if (!this.unit_seal(sand))
                             continue;
                         persisting.add(sand);
                         check_lord(sand.lord());
